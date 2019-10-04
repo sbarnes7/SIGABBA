88842ac8dc7d89c161f46c989382334e0560ac38
LukeM98 <37590202+LukeM98@users.noreply.github.com>
2019-10-04 12:48:29 -0400

    update rotors.js and html

--- v0.0.4/rotors.js
+++ v0.0.4/rotors.js
@@ -1,14 +1,15 @@
 // JavaScript source code
 
 
-var cipher1, cipher2, cipher3, cipher4,cipher5,control1,control2,control3, flag;
-
+var cipher1, cipher2, cipher3, cipher4,cipher5,control1,control2,control3,control4,control5, flag;
+var index1, index2, index3, index4, index5;
 
 
 
 function getPos(num) {
     var pos;
     var val;
+
     if (num == 0) {
         pos = document.getElementById("cipher1_pos");
     } else if (num == 1) {
@@ -21,7 +22,31 @@ function getPos(num) {
 
     } else if (num == 4) {
         pos = document.getElementById("cipher5_pos");
-    } 
+    } else if (num == 5) {
+        pos = document.getElementById("control1_pos");
+    } else if (num == 6) {
+        pos = document.getElementById("control2_pos");
+
+    } else if (num == 7) {
+        pos = document.getElementById("control3_pos");
+
+    } else if (num == 8) {
+        pos = document.getElementById("control4_pos");
+    } else if (num == 9) {
+        pos = document.getElementById("control5_pos");
+    } else if (num == 10) {
+        pos = document.getElementById("index1_pos");
+    } else if (num == 11) {
+        pos = document.getElementById("index2_pos");
+    } else if (num == 12) {
+        pos = document.getElementById("index3_pos");
+
+    } else if (num == 13) {
+        pos = document.getElementById("index4_pos");
+
+    } else if (num == 14) {
+        pos = document.getElementById("index5_pos");
+    }
     val = pos.options[pos.selectedIndex].value;
     return val;
 
@@ -64,9 +89,16 @@ function initialize() {
     cipher3 = getPos(2);
     cipher4 = getPos(3);
     cipher5 = getPos(4);
-
-
-
+    control1 = getPos(5);
+    control2 = getPos(6);
+    control3 = getPos(7);
+    control4 = getPos(8);
+    control5 = getPos(9);
+    index1 = getPos(10);
+    index2 = getPos(11);
+    index3 = getPos(12);
+    index4 = getPos(13);
+    index5 = getPos(14);
 }
 
 function print() {
@@ -78,32 +110,45 @@ function print() {
 
 function checkPos() {
 
-    var array =
-        [ cipher1, cipher2, cipher3, cipher4, cipher5 ];
+    var control_cipher_arr =
+        [ cipher1, cipher2, cipher3, cipher4, cipher5, control1, control2, control3, control4,control5];
+
+    var index_arr =
+        [index1, index2, index3, index4, index5];
 
     flag = 0;
-    for (var i = 0; i < 5; i++) {
+    for (var i = 0; i < control_cipher_arr.length; i++) {
 
-        for (var j = 0; j < 5; j++) {
+        for (var j = 0; j < control_cipher_arr.length; j++) {
             if (j != i) {
-                if (array[i] == array[j])
+                if (control_cipher_arr[i] == control_cipher_arr[j])
                     flag = 1;
             }
             }
         
     }
 
-   
-    /*for (var i = 0; i < 9; i++) {
+    for (var i = 0; i < index_arr.length; i++) {
 
-    }*/
+        for (var j = 0; j < index_arr.length; j++) {
+            if (j != i) {
+                if (index_arr[i] == index_arr[j])
+                    flag = 1;
+            }
+        }
+
+    }
 }
 
 
 function popup() {
 
     if (flag == 1) {
-        alert("fucc");
+        alert("Invalid Rotor Positions. Each Control/Cipher Rotor must have different rotor numbers. Same rule applies for the Index Rotors");
+        document.getElementById("link").className = "unactivated";
+    } else {
+
+        document.getElementById("link").className = "activated";
     }
 
 
